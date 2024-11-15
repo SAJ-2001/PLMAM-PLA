854d44a4e4a0811e782ebbbab1534eddd0cf5b59
SAJ-2001 <18012295638@163.com>
2024-11-15 18:43:41 +0800

    Update cross_attention.py

--- script/cross_attention.py
+++ script/cross_attention.py
@@ -6,7 +6,6 @@ from einops.layers.torch import  Reduce
 class FeedForwardNetwork(nn.Module):
     def __init__(self, hidden_size, ffn_size):
         super(FeedForwardNetwork, self).__init__()
-
         self.layer1 = nn.Linear(hidden_size, ffn_size)
         self.gelu = nn.ReLU(inplace=True)
         self.layer2 = nn.Linear(ffn_size, hidden_size)
@@ -21,22 +20,16 @@ class FeedForwardNetwork(nn.Module):
 class MultiHeadAttention(nn.Module):
     def __init__(self, hidden_size, attention_dropout_rate, num_heads):
         super(MultiHeadAttention, self).__init__()
-
         self.num_heads = num_heads
-
         self.att_size = att_size = hidden_size // num_heads
         self.scale = att_size ** -0.5
-
         self.linear_q = nn.Linear(hidden_size, num_heads * att_size)
         self.linear_k = nn.Linear(hidden_size, num_heads * att_size)
         self.linear_v = nn.Linear(hidden_size, num_heads * att_size)
         self.att_dropout = nn.Dropout(attention_dropout_rate)
-
         self.output_layer = nn.Linear(num_heads * att_size, hidden_size)
 
     def forward(self, q, k, v, attn_bias=None):
-
-
         d_k = self.att_size
         d_v = self.att_size
         batch_size = q.size(0)
@@ -56,28 +49,21 @@ class MultiHeadAttention(nn.Module):
         # if attn_bias is not None:
         #     x = x + attn_bias
         x = torch.softmax(x, dim=3)
-
         x = self.att_dropout(x)
         x = x.matmul(v)  # [b, h, q_len, attn]
-        
         x = x.transpose(1, 2).contiguous()  # [b, q_len, h, attn]
         x = x.view(batch_size, -1, self.num_heads * d_v)
-        
         x = self.output_layer(x)
-        
-
         return x
 
 
 class EncoderLayer(nn.Module):
     def __init__(self, hidden_size, ffn_size, dropout_rate, attention_dropout_rate, num_heads):
         super(EncoderLayer, self).__init__()
-
         self.self_attention_norm = nn.LayerNorm(hidden_size)
         self.self_attention = MultiHeadAttention(
             hidden_size, attention_dropout_rate, num_heads)
         self.self_attention_dropout = nn.Dropout(dropout_rate)
-
         self.ffn_norm = nn.LayerNorm(hidden_size)
         self.ffn = FeedForwardNetwork(hidden_size, ffn_size)
         self.ffn_dropout = nn.Dropout(dropout_rate)
